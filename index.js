835cf5c0759774cab253ba7ff2ba07e9e90ca6a7
konda ravilla <kondaravilla000@gmail.com>
2024-02-09 14:57:31 +0530

    variables & typeof operator

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+
+var a = 'sachin'
+
+console.log(a, typeof a)
+
+var b = 40
+console.log(b, typeof b)
+
+var c = true
+console.log(c, typeof c)
+
+var d
+console.log(d, typeof d)
+
+var e = null
+console.log(e, typeof e)
+
+var f = { name: 'kohli' }
+console.log(f, typeof f)
+
+var g = [1, 2, 3]
+
+console.log(g, typeof g)
+console.log(Array.isArray(g))
+
+var h = function () {
+  var x = 100
+}
+// console.log(x)
+console.log(h)
+h()
+console.log(h, typeof h)
+
+var i = function () {
+  var y = 200
+}
+i()
+
