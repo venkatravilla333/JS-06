a4e14edf9f26efc9e8241685d4c91679a9c00087
konda ravilla <kondaravilla000@gmail.com>
2024-03-03 13:23:53 +0530

    async js started

---
+++ Async-js/index.js
@@ -0,0 +1,32 @@
+// console.log('one')
+// console.log('two')
+
+// setTimeout(() => {
+//   console.log('timer-1')
+// }, 2000)
+
+// console.log('three')
+
+// Promise.resolve().then(() => console.log('pr-1')).then(() => console.log('pr-2'))
+
+// setTimeout(() => {
+//   console.log('timer-2')
+// }, 0)
+
+// console.log('bye')
+
+
+function fetchData() {
+  setTimeout(() => {
+    var data = 'sachin'
+  }, 4000)
+}
+
+fetchData()
+
+
+function displayData(data) {
+  console.log(data)
+}
+
+displayData()
