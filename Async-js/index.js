20c43b3b0a96e6389056783f250bc291765735e1
konda ravilla <kondaravilla000@gmail.com>
2024-03-04 13:55:12 +0530

    promises & asyn await

--- Async-js/index.js
+++ Async-js/index.js
@@ -15,18 +15,98 @@
 
 // console.log('bye')
 
+// callback function
 
-function fetchData() {
-  setTimeout(() => {
-    var data = 'sachin'
-  }, 4000)
+// function fetchData(num, displayData ) {
+//   setTimeout(() => {
+//     // var data = {
+//     //   pName: 'Apple',
+//     //   pPrice: 100
+//     // }
+//    var data = num*num
+//     displayData(data)
+//   }, 4000)
+// }
+
+// function displayData(data, multifly) {
+//   console.log(data)
+//   console.log(multifly)
+// }
+
+// fetchData(2, (data1) => {
+//   console.log(data1)
+//   fetchData(data1, (data2) => {
+//     console.log(data2)
+//     fetchData(data2, (data3) => {
+//       console.log(data3);
+//       fetchData(data3, (data4) => {
+//         console.log(data4);
+//         fetchData(data4, (data5) => {
+//           console.log(data5);
+//           fetchData(data5, (data6) => {
+//             console.log(data6);
+//           });
+//         });
+//       });
+//     });
+//   })
+// })
+
+
+// Promises
+
+// function fetchData(num) {
+//   var Pr = new Promise((res, rej) => {
+//    setTimeout(() => {
+//       // var data = { pName: 'Apple', pPrice: 100 };
+//       var data = num*num
+//      // console.log(data);
+//      res(data)
+//     }, 4000);
+//   })
+//   return Pr
+  
+// }
+// fetchData(2).then((data1) => {
+//   console.log(data1)
+//  return fetchData(data1)
+// }).then((data2) => {
+//   console.log(data2)
+//  return fetchData(data2)
+// }).then((data3) => {
+//   console.log(data3)
+//   return fetchData(data3)
+// }).then((data4) => {
+//   console.log(data4)
+// })
+
+//Async await
+
+function fetchData(num) {
+  var prResult = new Promise((res, rej) => {
+    setTimeout(() => {
+      // var data = { pName: 'Apple', pPrice: 100 }
+      var data = num*num
+      res(data)
+    }, 4000)
+  })
+  return prResult
 }
 
-fetchData()
 
 
-function displayData(data) {
-  console.log(data)
+async function displayData() {
+  var data1 = await fetchData(2); //4000 //async
+  console.log(data1); //syc
+  var data2 = await fetchData(data1); //4000 //async
+  console.log(data2); //syc
+  var data3 = await fetchData(data2); //4000 //async
+  console.log(data3); //syc
+  var data4 = await fetchData(data3); //4000 //async
+  console.log(data4); //syc
 }
-
 displayData()
+
+
+
+
