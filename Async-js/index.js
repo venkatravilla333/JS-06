d05d057362c6bec4f6700e0d867bbe3f7e02163b
konda ravilla <kondaravilla000@gmail.com>
2024-03-05 13:05:57 +0530

    api calls

--- Async-js/index.js
+++ Async-js/index.js
@@ -82,30 +82,126 @@
 
 //Async await
 
-function fetchData(num) {
-  var prResult = new Promise((res, rej) => {
-    setTimeout(() => {
-      // var data = { pName: 'Apple', pPrice: 100 }
-      var data = num*num
-      res(data)
-    }, 4000)
+// function fetchData(num) {
+//   var prResult = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       // var data = { pName: 'Apple', pPrice: 100 }
+//       var data = num*num
+//       res(data)
+//     }, 4000)
+//   })
+//   return prResult
+// }
+
+
+
+// async function displayData() {
+//   var data1 = await fetchData(2); //4000 //async
+//   console.log(data1); //syc
+//   var data2 = await fetchData(data1); //4000 //async
+//   console.log(data2); //syc
+//   var data3 = await fetchData(data2); //4000 //async
+//   console.log(data3); //syc
+//   var data4 = await fetchData(data3); //4000 //async
+//   console.log(data4); //syc
+// }
+// displayData()
+
+
+//API CALLS
+
+// GET Request
+
+
+var getdataBtn = document.getElementById('getdata')
+var getsingleitem = document.getElementById('getsingleitem')
+var senddataBtn = document.getElementById('senddata')
+var deletedataBtn = document.getElementById('deletedata')
+var updatedataBtn = document.getElementById('updatedata')
+
+getdataBtn.addEventListener('click', fetchData)
+getsingleitem.addEventListener('click', fetchSingleItem)
+senddataBtn.addEventListener('click', sendData)
+deletedataBtn.addEventListener('click', deleteData)
+updatedataBtn.addEventListener('click', updateData)
+
+function fetchData() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => {
+      console.log(res)
+    return res.json()
+    }).then((data) => {
+    console.log(data)
+    }).catch((error) => {
+    console.log(error)
   })
-  return prResult
+}
+function fetchSingleItem() {
+  console.log('hello')
+  fetch('https://jsonplaceholder.typicode.com/posts/10')
+    .then((res) => {
+      console.log(res)
+    return res.json()
+    }).then((data) => {
+    console.log(data)
+    }).catch((error) => {
+    console.log(error)
+  })
+}
+
+var data = {
+  userId: 200,
+  title: 'My title',
+  body: 'My body'
+};
+
+function sendData() {
+  fetch('https://jsonplaceholder.typicode.com/posts',  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => {
+     console.log(res)
+    }).catch((error) => {
+    console.log(error)
+  })
+}
+
+function deleteData() {
+  fetch('https://jsonplaceholder.typicode.com/posts/20', {
+    method: 'DELETE',
+    // headers: {
+    //   'Content-Type': 'application/json',
+    // },
+    // body: JSON.stringify(data),
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 
-
-async function displayData() {
-  var data1 = await fetchData(2); //4000 //async
-  console.log(data1); //syc
-  var data2 = await fetchData(data1); //4000 //async
-  console.log(data2); //syc
-  var data3 = await fetchData(data2); //4000 //async
-  console.log(data3); //syc
-  var data4 = await fetchData(data3); //4000 //async
-  console.log(data4); //syc
+function updateData() {
+  fetch('https://jsonplaceholder.typicode.com/posts/20', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-displayData()
 
 
 
