ef59d399e820f0081e56891e5252e043e8731e87
konda ravilla <kondaravilla000@gmail.com>
2024-03-14 13:37:18 +0530

    recursion & currying & IIFE

--- Mis-topics/index.js
+++ Mis-topics/index.js
@@ -38,3 +38,80 @@
 // impure(1,2,3)
 
 
+
+// Recursion function
+
+// function factorial(num) {
+
+//   if (num <= 0) {
+//     return 1
+//   } else {
+//     return num*factorial(num-1)
+//   }
+  
+// }
+
+// console.log(factorial(3))
+
+
+//Function currying
+
+
+// function withoutCurry(a,b,c) {
+//   console.log(a+b+c)
+// }
+
+// withoutCurry(1, 2, 3)
+
+
+// function withCurryA(a) {
+//   return function withCurryB(b){
+//     return function withCurryC(c) {
+//       console.log(a+b+c)
+//     }
+//   }
+// }
+
+// withCurryA(1)(2)(3)
+
+
+
+// function calculateDis(price, dis) {
+//   console.log(price*dis)
+// }
+
+// calculateDis(1000, 0.1)
+// calculateDis(1000, 0.2)
+// calculateDis(1000, 0.3)
+// calculateDis(1000, 0.4)
+// calculateDis(1000, 0.5)
+
+
+// function calprice(price) {
+//   return function caldiscount(dis) {
+//     console.log(price*dis)
+//   }
+
+// }
+
+// var x = calprice(1000)
+// console.log(caldiscount)
+// caldiscount(.1)
+// caldiscount(.2)
+// caldiscount(.3)
+// caldiscount(.4)
+// caldiscount(.5)
+
+
+//IIFE
+
+
+// var b = 200;
+
+// (() => {
+//   var a = 100
+//   console.log(a)
+// })()
+
+// console.log(b)
+// console.log(a)
