e69da383e7b7427c135a55b3d6e82d28eabba803
konda ravilla <kondaravilla000@gmail.com>
2024-03-19 13:42:13 +0530

    type-conversion

---
+++ Type-conversion/explicit/explicit.js
@@ -0,0 +1,21 @@
+
+
+let x = 10
+
+console.log(x)
+console.log(typeof x)
+
+let y = String(x)
+
+console.log(y)
+console.log(typeof y)
+
+var a = true
+
+console.log(a)
+console.log(typeof a)
+
+var b = Number(a)
+
+console.log(b)
+console.log(typeof b)
---
+++ Type-conversion/implicit/implicit.js
@@ -0,0 +1,47 @@
+
+//numeric string with + operator
+console.log('4' + 4)
+console.log('4' + 'hi')
+console.log('4' + undefined)
+console.log('4' + null)
+console.log('4' + true)
+
+
+//numeric string with - / % * operator
+
+console.log('4' - 4);
+console.log('4' - 'hi');
+console.log(4 - false);
+console.log('4' - null);
+console.log('4' - undefined);
+
+//non numeric string with - / % * operator
+
+console.log(true - false);
+console.log(null - undefined);
+console.log(null - 'hi');
+
+// boolean (true=1) (false=0)
+
+console.log(!true - 'hi')
+console.log(false - 'hi')
+console.log(false - null)
+console.log(false - undefined)
+console.log(true - undefined)
+
+//null (null=0)
+
+console.log(null + undefined)
+console.log(null + true)
+console.log(null + false)
+
+// undefined + - % / *
+
+console.log(undefined + 'hi')
+console.log(undefined - 'hi')
+console.log(undefined - undefined)
+console.log(undefined - true)
+console.log(undefined - false)
+
+
+
