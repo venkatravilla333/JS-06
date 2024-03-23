95178bf074d06eeede4d067e58e7ec1d85fe11a4
konda ravilla <kondaravilla000@gmail.com>
2024-03-23 14:05:37 +0530

    bom end

--- Bom/bom.js
+++ Bom/bom.js
@@ -23,39 +23,82 @@
 // }
 
 
-var openbtn = window.document.getElementById('open')
-var closebtn = window.document.getElementById('close')
-var movebtn = window.document.getElementById('move')
-var resizebtn = window.document.getElementById('resize')
+// var openbtn = window.document.getElementById('open')
+// var closebtn = window.document.getElementById('close')
+// var movebtn = window.document.getElementById('move')
+// var resizebtn = window.document.getElementById('resize')
 
-var mywindow
+// var mywindow
 
-function openWindow() {
- mywindow = window.open('', 'hello', 'width=200,height=100');
-}
+// function openWindow() {
+//  mywindow = window.open('', 'hello', 'width=200,height=100');
+// }
 
-function closeWindow() {
-  mywindow.close();
-}
+// function closeWindow() {
+//   mywindow.close();
+// }
 
-function moveWindow() {
- return mywindow.moveTo(600, 500);
-}
+// function moveWindow() {
+//  return mywindow.moveTo(600, 500);
+// }
 
-function resizeWindow() {
- return mywindow.resizeTo(600,100);
-}
+// function resizeWindow() {
+//  return mywindow.resizeTo(600,100);
+// }
 
-openbtn.addEventListener('click', openWindow)
-closebtn.addEventListener('click', closeWindow)
-movebtn.addEventListener('click', moveWindow)
-resizebtn.addEventListener('click', resizeWindow)
+// openbtn.addEventListener('click', openWindow)
+// closebtn.addEventListener('click', closeWindow)
+// movebtn.addEventListener('click', moveWindow)
+// resizebtn.addEventListener('click', resizeWindow)
 
 
 //window obj properties
-// console.log(window.innerWidth)
-// console.log(window.innerHeight)
+console.log(window.innerWidth)
+console.log(window.innerHeight)
 
 //screen object properties
+console.log(window.screen.width)
+console.log(window.screen.height)
+
+//Navigator object
+
+// console.log(window.navigator.appName)
+// console.log(window.navigator.platform)
+// console.log(window.navigator.language)
+// console.log(window.navigator.onLine)
+
+//Screen object
+
 // console.log(window.screen.width)
 // console.log(window.screen.height)
+
+//Location object
+
+// console.log(window.location.href)
+// console.log(window.location.hostname)
+// console.log(window.location.pathname)
+// console.log(window.location.protocol)
+// console.log(window.location.port)
+
+//History object
+
+console.log(window.history)
+
+
+let forwardbtn = document.getElementById('forward')
+console.log(forwardbtn)
+
+function forwardFun() {
+//  console.log(window.history.length)
+ console.log(window.history.forward('./forward.html'))
+}
+
+// forwardbtn.addEventListener('click', forwardFun)
+
+let backBtn = document.getElementById('back')
+
+function backFun() {
+ console.log(window.history.back('./bom.html'))
+}
+
+backBtn.addEventListener('click', backFun)
